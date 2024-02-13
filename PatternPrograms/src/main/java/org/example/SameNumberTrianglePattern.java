44a7a615486399cdfc8a6157f32bafd237f45680
prakashg30 <prakashg@crayondata.com>
2024-02-13 14:52:20 +0530

    newfile

---
+++ PatternPrograms/src/main/java/org/example/SameNumberTrianglePattern.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class SameNumberTrianglePattern {
+    public static void main(String args[]){
+
+        System.out.println("Inside Main -- enter count number : ");
+        Scanner count = new Scanner(System.in);
+        int n = count.nextInt();
+        pattern(n);
+    }
+    public static void pattern(int n){
+        int j;
+        for (int i=1;i<=n;i++){
+            for(j=1;j<=n-i;j++) {
+                System.out.print(" ");
+            }
+                for (j =1;j<i;j++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    }
+
